ebb67f304c985f14a482c851a399b882d086d1c6
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2021-10-11 18:13:25 +0100

    Write home page tests

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,21 @@
+from django.http import response
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class HomePageTests(TestCase):
+    """ Test the homepage functionality"""
+
+    def test_home_page(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_contents(self):
+        response = self.client.get("/")
+        self.assertContains(response, "Hacker News")
+
+    def test_home_page_template(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home')
